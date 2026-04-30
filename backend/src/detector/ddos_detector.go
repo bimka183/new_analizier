c40db593bbe5dfc8c2626d89c072352438220fe0
pipinstallsympy <misha.zherbin.05@gmail.com>
2026-04-30 14:12:07 +0300

    Cleanup after previous programmer

--- backend/src/detector/ddos_detector.go
+++ backend/src/detector/ddos_detector.go
@@ -23,7 +23,7 @@ func (d *DDoSDetector) AnalyzeWindows(windows []packet.TimeWindow) []packet.Time
 	const (
 		bpsThreshold      = 1_000_000
 		rstSynRatio       = 15.0
-		uniqueDstPortsAbs = 371
+		uniqueDstPortsAbs = 370
 		highAvgPorts      = 1000
 		windowCount       = 10
 	)
@@ -33,34 +33,34 @@ func (d *DDoSDetector) AnalyzeWindows(windows []packet.TimeWindow) []packet.Time
 		return anomalous
 	}
 
-	for i, w := range windows {
-		s := w.Stats
+	for i, windowElement := range windows {
+		stats := windowElement.Stats
 
-		// 1. Аномальное отношение RST/SYN
-		if s.CntSYN > 0 && float64(s.CntRST)/float64(s.CntSYN) > rstSynRatio {
-			anomalous = append(anomalous, w)
+		if stats.BPS <= bpsThreshold {
 			continue
 		}
 
-		if s.BPS <= bpsThreshold {
+		// 1. Аномальное отношение RST/SYN
+		if stats.CntSYN > 0 && float64(stats.CntRST)/float64(stats.CntSYN) > rstSynRatio {
+			anomalous = append(anomalous, windowElement)
 			continue
 		}
 
 		// 2. Абсолютное большое количество уникальных портов назначения
-		if s.UniqueDstPorts > uniqueDstPortsAbs {
-			anomalous = append(anomalous, w)
+		if stats.UniqueDstPorts > uniqueDstPortsAbs {
+			anomalous = append(anomalous, windowElement)
 			continue
 		}
 
 		// 3. Долгосрочное высокое среднее уникальных портов
-		if i >= windowCount-1 {
+		if i > windowCount {
 			avg := 0.0
 			for j := i - windowCount + 1; j <= i; j++ {
 				avg += float64(windows[j].Stats.UniqueDstPorts)
 			}
 			avg /= windowCount
 			if avg > highAvgPorts {
-				anomalous = append(anomalous, w)
+				anomalous = append(anomalous, windowElement)
 			}
 		}
 	}
